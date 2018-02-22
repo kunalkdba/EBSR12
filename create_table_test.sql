aa60cef076ce3181441f64fe99a10a84668aa8e3
Kunal <kunalk.dba@gmail.com>
2018-02-22 08:27:11 +0530

    hi

--- create_table_test.sql
+++ create_table_test.sql
@@ -1,4 +1,4 @@
 --create table emp1(name varchar(20), age number (10));
 create table emp5(name varchar(20), age number (12));
 
-insert into emp4 values ('praveen',24);
+insert into emp4 values ('praveen',26);
